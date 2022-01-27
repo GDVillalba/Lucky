dc28917e5401b65ec52c3886d3d086b422096a65
GDVillalba <guilledvil@gmail.com>
2022-01-27 09:36:57 -0300

    Creados Modelo y Vista de Categorias, con carga de datos desde base de datos SQL ServerAgregar archivos de proyecto.

---
+++ Lucky/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using Lucky.Data;
+using Lucky.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lucky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db; 
+
+        public CategoryController(ApplicationDbContext db) //database context is passed from the service configured in startup
+        {
+            _db = db;   //copy of database context
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Categories;
+            return View(objList);
+        }
+    }
+}
